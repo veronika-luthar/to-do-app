4e785ca233ba4fb874bfa1739a26db1e64724fe8
veronika-luthar <justapinglet@gmail.com>
2024-02-15 12:06:07 +1300

    Created header visual

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.todoapp.ui.theme.ToDoAppTheme
 
@@ -22,7 +29,8 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val AppIcons = Icons.Rounded
+                    Header(AppIcons.Delete, AppIcons.Add)
                 }
             }
         }
@@ -30,17 +38,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Header(deleteIcon: ImageVector, addIcon: ImageVector, modifier: Modifier = Modifier) {
+    Row {
+        Icon(
+            imageVector = deleteIcon,
+            contentDescription = "Trash can symbol"
+        )
+        Text(
+            text = "TO-DO"
+        )
+        Icon(
+            imageVector = addIcon,
+            contentDescription = "Plus symbol"
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToDoAppTheme {
-        Greeting("Android")
+        val AppIcons = Icons.Rounded
+        Header(AppIcons.Delete, AppIcons.Add)
     }
 }
