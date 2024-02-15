87e8f36233e663fdbe3d538fbf39bfc597f42dfa
veronika-luthar <justapinglet@gmail.com>
2024-02-16 09:25:08 +1300

    Created Item composable to add to List

--- app/src/main/java/com/example/todoapp/MainActivity.kt
+++ app/src/main/java/com/example/todoapp/MainActivity.kt
@@ -3,13 +3,19 @@ package com.example.todoapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material.icons.rounded.Delete
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults.buttonColors
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -20,7 +26,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.todoapp.ui.theme.ToDoAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,8 +41,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val AppIcons = Icons.Rounded
-                    Header(AppIcons.Delete, AppIcons.Add)
+                    ToDoAppUI(Modifier)
                 }
             }
         }
@@ -44,10 +51,12 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Header(deleteIcon: ImageVector, addIcon: ImageVector, modifier: Modifier = Modifier) {
     val buttonColors = buttonColors(containerColor = Color.Transparent, contentColor = Color.Black)
-
     // Header
-    Row {
+    Row (
+        modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
 
+    ) {
         // Delete button
         Button(
             onClick = {},
@@ -59,13 +68,11 @@ fun Header(deleteIcon: ImageVector, addIcon: ImageVector, modifier: Modifier = M
                 contentDescription = "Trash can symbol"
             )
         }
-
         // TO-DO text
         Text(
             text = "TO-DO",
             modifier = Modifier.align(alignment = Alignment.CenterVertically)
         )
-        
         // Add button
         Button(
             onClick = {},
@@ -81,11 +88,69 @@ fun Header(deleteIcon: ImageVector, addIcon: ImageVector, modifier: Modifier = M
     }
 }
 
+@Composable
+fun Item(text: String, completed: Boolean, modifier: Modifier = Modifier){
+    Row(
+        modifier = modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+        Text(
+            text = text,
+            textDecoration = if (completed) {
+                TextDecoration.LineThrough
+            } else {
+                null
+            },
+            modifier = modifier.padding(5.dp)
+        )
+        Checkbox(
+            checked = completed,
+            onCheckedChange = {},
+
+        )
+    }
+    Divider(
+        thickness = 2.dp,
+        color = Color.Black,
+        modifier = modifier.fillMaxWidth(4/5f)
+    )
+}
+
+@Composable
+fun List(modifier: Modifier = Modifier){
+
+    // List
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.Start
+        ){
+        for(i in 1..10){
+            Item("Incomplete Item", false, modifier)
+        }
+    }
+}
+
+@Composable
+fun ToDoAppUI(modifier: Modifier = Modifier){
+    val AppIcons = Icons.Rounded
+    Column (
+        modifier = modifier.fillMaxSize(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+
+    ) {
+        Header(AppIcons.Delete, AppIcons.Add, modifier)
+        Divider(
+            thickness = 2.dp,
+            color = Color.Black
+        )
+        List(modifier)
+    }
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ToDoAppTheme {
-        val AppIcons = Icons.Rounded
-        Header(AppIcons.Delete, AppIcons.Add)
+        ToDoAppUI(Modifier)
     }
 }
